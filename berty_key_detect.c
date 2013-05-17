99e8da22ded4e72b54d3d2b88f7cd0e362e26d12
Rezl <resslx@gmail.com>
2013-05-17 18:25:51 -0500

    Input tap with toggle test1
    The print (output in shell when the command is pressed) for the

--- berty_key_detect.c
+++ berty_key_detect.c
@@ -45,17 +45,12 @@ int main(int argc, char **argv){
 					case 17:
 						show_char("LEFT ARM");
 						// PUNCH
-						send_event(4,80);
+						send_event(12,273);
 						break;
 					case 30:
 						show_char("RIGHT ARM");
 						// INTERACT
-						if(ev.value == 1){
-							system("sendevent /dev/input/event0 3 57 46; sendevent /dev/input/event0 3 53 381; sendevent /dev/input/event0 3 54 2111; sendevent /dev/input/event0 0 0 0");
-						}
-						else{
-							system("sendevent /dev/input/event0 3 57 4294967295; sendevent /dev/input/event0 0 0 0");
-						}
+						send_event(12,237);
 						break;
 					case 31:
 						show_char("RIGHT NIPPLE");
@@ -80,22 +75,12 @@ int main(int argc, char **argv){
 					case 33:
 						show_char("LEFT LEG");
 						// MOVE RIGHT
-						if(ev.value == 1){
-							system("sendevent /dev/input/event0 3 57 43; sendevent /dev/input/event0 3 53 239; sendevent /dev/input/event0 3 54 2101; sendevent /dev/input/event0 0 0 0");
-						}
-						else{
-							system("sendevent /dev/input/event0 3 57 4294967295; sendevent /dev/input/event0 0 0 0");
-						}
+						send_event(12,135);
 						break;
 					case 34:
 						show_char("LEFT NIPPLE");
 						// JUMP
-						if(ev.value == 1){
-							system("sendevent /dev/input/event0 3 57 45; sendevent /dev/input/event0 3 58 3; sendevent /dev/input/event0 3 53 327; sendevent /dev/input/event0 0 0 0");
-						}
-						else{
-							system("sendevent /dev/input/event0 3 57 4294967295; sendevent /dev/input/event0 0 0 0");
-						}
+						send_event(12,204);
 						break;
 					case 57:
 						show_char("EAR");
